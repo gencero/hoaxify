5d87faa994b6d6378a73debba0992446be814de6
orhan gencer <o.gencer@yahoo.com>
2020-04-25 01:25:03 +0300

    0205-genericresponse and userservice class added. refactoring code done.

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -3,9 +3,13 @@ package com.hoaxify.ws.user;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import shared.GenericResponse;
 
 @RestController
 public class UserController {
@@ -13,12 +17,13 @@ public class UserController {
 	//private static final Logger log = LoggerFactory.getLogger(UserController.class);
 	
 	@Autowired
-	UserRepository userRepository;
+	UserService userService;
 
 	@PostMapping("/api/1.0/users")
-	public void createUser(@RequestBody User user) {
+	public GenericResponse createUser(@RequestBody User user) {
 		//log.info("user: " + user.toString());
-		userRepository.save(user);
+		userService.save(user);
+		return new GenericResponse("User created");
 	}
 
 }
