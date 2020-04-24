3d2d1c22698d84b7d383dcd8fc5a996ee18fc765
orhan gencer <o.gencer@yahoo.com>
2020-04-24 10:32:49 +0300

    0204-database settings and user saving db

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -2,6 +2,7 @@ package com.hoaxify.ws.user;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 	
+	//private static final Logger log = LoggerFactory.getLogger(UserController.class);
 	
-	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	@Autowired
+	UserRepository userRepository;
 
 	@PostMapping("/api/1.0/users")
 	public void createUser(@RequestBody User user) {
-		log.info("user: " + user.toString());
-		
+		//log.info("user: " + user.toString());
+		userRepository.save(user);
 	}
 
 }
