18a430e762a391b80c0e3334efcdddec09303e12
orhan gencer <o.gencer@yahoo.com>
2020-04-26 18:45:26 +0300

    0401-backend validations language support

--- ws/src/main/java/com/hoaxify/ws/user/User.java
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -17,7 +17,7 @@ public class User {
 	@GeneratedValue
 	private long id;
 	
-	@NotNull
+	@NotNull(message = "{hoaxify.constraints.username.NotNull.message}")
 	@Size(min = 4, max = 255)
 	@UniqueUsername
 	private String username;
@@ -27,7 +27,7 @@ public class User {
 	private String displayName;
 	
 	@NotNull
-	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "{hoaxify.constraints.password.Pattern.message}" )
 	@Size(min = 8)
 	private String password;	
 	
