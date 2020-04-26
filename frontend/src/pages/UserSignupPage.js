e611d86d16e22d8a934bbf75b956e08e163a7fd2
orhan gencer <o.gencer@yahoo.com>
2020-04-26 23:15:18 +0300

    0403-toggling language

--- frontend/src/pages/UserSignupPage.js
+++ frontend/src/pages/UserSignupPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {signup} from "../api/apiCalls";
+import {signup, changeLanguage } from "../api/apiCalls";
 import Input from "../components/Input";
 import { withTranslation } from 'react-i18next';
 
@@ -19,10 +19,10 @@ class UserSignupPage extends React.Component {
     const errors = {...this.state.errors};//object'nin kopyası olusturuldu
     errors[name] = undefined;
 
-    if (name == 'password' || name == 'passwordRepeat'){
-      if (name == 'password' && value != this.state.passwordRepeat){
+    if (name === 'password' || name === 'passwordRepeat'){
+      if (name === 'password' && value !== this.state.passwordRepeat){
         errors.passwordRepeat = t('Password mismatch');
-      }else if (name == 'passwordRepeat' && value != this.state.password) {
+      }else if (name === 'passwordRepeat' && value !== this.state.password) {
         errors.passwordRepeat = t('Password mismatch');
       }else {
         errors.passwordRepeat = undefined;
@@ -71,6 +71,12 @@ class UserSignupPage extends React.Component {
     //});
   };
 
+  onChangeLanguage = language => {
+    const { i18n } = this.props;
+    i18n.changeLanguage(language);
+    changeLanguage(language);
+  }
+
   render() {
     const {pendingApiCall, errors} = this.state;
     const {username, displayName, password, passwordRepeat } = errors;
@@ -80,15 +86,29 @@ class UserSignupPage extends React.Component {
       <div className="container">
         <form>
           <h1 className="text-center">{t('Sign Up')}</h1>
-          <Input name="username" label= {t("Username")} error= {username} onChange = {this.onChange} />
-          <Input name="displayName" label= {t("Display Name")} error= {displayName} onChange = {this.onChange} />
-          <Input name="password" label= {t("Password")} error= {password} onChange = {this.onChange} type="password" />
-          <Input name="passwordRepeat" label= {t("Password Repeat")} error= {passwordRepeat} onChange = {this.onChange} type="password" />
+          <Input name="username" label= {t('Username')} error= {username} onChange = {this.onChange} />
+          <Input name="displayName" label= {t('Display Name')} error = {displayName} onChange = {this.onChange} />
+          <Input name="password" label= {t('Password')} error = {password} onChange = {this.onChange} type="password" />
+          <Input name="passwordRepeat" label= {t('Password Repeat')} error= {passwordRepeat} onChange = {this.onChange} type="password" />
           <div className="text-center">
-            <button className="btn btn-primary" disabled={pendingApiCall || passwordRepeat != undefined} onClick={this.onClickSignup}>
+            <button className="btn btn-primary" disabled={pendingApiCall || passwordRepeat !== undefined} onClick={this.onClickSignup}>
               {t('Sign Up')}
               {pendingApiCall && <span className="spinner-border spinner-border-sm"></span>}
             </button>
+          </div>
+          <div>
+            <img
+              src="https://www.countryflags.io/tr/flat/24.png"
+              alt="Turkish Flag"
+              onClick={() => this.onChangeLanguage('tr')}
+              style={{ cursor: 'pointer' }}
+            ></img>
+            <img
+              src="https://www.countryflags.io/us/flat/24.png"
+              alt="USA Flag"
+              onClick={() => this.onChangeLanguage('en')}
+              style={{ cursor: 'pointer' }}
+            ></img>
           </div>
         </form>
       </div>
