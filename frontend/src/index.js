7a221a613c3001228a3a1e33b1180bbd44489883
orhan gencer <o.gencer@yahoo.com>
2020-04-27 17:10:49 +0300

    0502-LanguageSelector component create and login page translations

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,11 +7,15 @@ import UserSignupPage from './pages/UserSignupPage';
 import LoginPage from './pages/LoginPage';
 import * as serviceWorker from './serviceWorker';
 import './i18n';
+import LanguageSelector from './components/LanguageSelector';
 
 ReactDOM.render(
+  <div>
   <React.StrictMode>
     <LoginPage />
-  </React.StrictMode>,
+    <LanguageSelector />
+  </React.StrictMode>
+  </div>,
   document.getElementById('root')
 );
 
